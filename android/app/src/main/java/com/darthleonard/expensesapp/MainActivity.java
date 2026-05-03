329ac46a7b10fefac6135647f8df8492467a120c
darthleonard <leolinuxmx@gmail.com>
2026-05-03 16:01:02 -0600

    add Android

---
+++ android/app/src/main/java/com/darthleonard/expensesapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.darthleonard.expensesapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.darthleonard.expensesapp',
   appName: 'expense-app',
   webDir: 'www'
 };
