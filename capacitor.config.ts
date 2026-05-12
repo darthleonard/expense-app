884f18d4861b062f14d08819117cc3d7c88be6cc
darthleonard <leolinuxmx@gmail.com>
2026-05-11 21:15:28 -0600

    feat: initialize web index template and configure native application branding metadata

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.darthleonard.expensesapp',
-  appName: 'expense-app',
+  appName: 'Expense App',
   webDir: 'www'
 };
 
